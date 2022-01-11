f25a0158a3b35cb51c058666d49cd6000dc16c92
Daniela Peraza <danielaperazav@gmail.com>
2022-01-11 14:48:09 -0500

    bucles

---
+++ bucles/Frizz Buzz.js
@@ -0,0 +1,12 @@
+let numero = Number(prompt("Ingrese un número entero"))
+let h1numeroentero = document.querySelector("#respuestanumero")
+ 
+if(numero%5 ==0 && numero%3) {
+    h1respuestanumero.nnetHTML = "Frizzbuzz"}
+else if(numero%5) {
+    h1respuestanumero.nnetHTML = ("Buzz") }
+    else if(numero%3) {
+        h1respuestanumero.nnetHTML = ("Fizz") }
+ 
+console.log(h1respuestanumero)
+
---
+++ condicionales/condicionales2.js
@@ -0,0 +1,8 @@
+let numero = Number(prompt("Ingrese el número"))
+
+if(numero % 3 == 0)
+{console.log ("Su número es divisible entre 3")
+}else{
+if(numero % 5 == 0)
+{console.log ("Su número es divisible entre 5")
+}else{
